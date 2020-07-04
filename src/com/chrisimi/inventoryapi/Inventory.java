4561e0d099efb0ff36a3e6b4b6f9c854adfa97f1
chrisimi <chrisi050403@gmail.com>
2020-07-04 14:17:01 +0200

    changed vom JavaPlugin to plugin

--- src/com/chrisimi/inventoryapi/Inventory.java
+++ src/com/chrisimi/inventoryapi/Inventory.java
@@ -15,6 +15,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -34,10 +35,10 @@ public class Inventory {
 
 
 	private final UUID InventoryID;
-	private final JavaPlugin plugin;
+	private final Plugin plugin;
 
 	@Deprecated
-	public Inventory(Player player, int size, JavaPlugin plugin, String inventoryName) {
+	public Inventory(Player player, int size, Plugin plugin, String inventoryName) {
 		this.player = player;
 		this.bukkitInventory = Bukkit.createInventory(player, size, inventoryName);
 		this.InventoryID = UUID.randomUUID();
